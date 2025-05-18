b4a2b4c56dafc4f1b17f3632221f3cb26a271bb2
Girdhari <girdharigodara007@gmail.com>
2025-05-18 15:29:24 +0000

    variables

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,12 @@
+const AccountId = 12345
+let AccountMail = "girdhari@gmail.com"
+var AccountPh = "9928451734"
+AccountCity = "Delhi"
+
+//const can't be changed
+AccountMail = "god@gmail.com"
+AccountPh = "1234567890"
+AccountCity = "Bombey"
+
+console.log(AccountCity);
+console.table([AccountId, AccountMail, AccountPh, AccountCity])
